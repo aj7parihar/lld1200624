3214dcf83e07e28087f21814ae98e62bd4b8bcc6
Ajay Singh Parihar <AParihar@quinnox.com>
2024-07-17 01:41:48 +0530

    Class:090724 (lld1class11) - Collection Framework & overview, ArrayList, LL, HashSet, LinkedHashSet backend implementation wrto addition and removal of elements and their TC, Amortized TC, PriorityQueue or Heap implementation, Comparable Interface implemation for any object along with PQ.

---
+++ src/main/java/in/aj7parihar/lldoops2200624/lld1class11/collections/Car.java
@@ -0,0 +1,28 @@
+package in.aj7parihar.lldoops2200624.lld1class11.collections;
+
+public class Car implements Comparable<Car>{
+    private String model;
+    private int price;
+    private int maxSpeed;
+
+    public Car(String model, int price, int maxSpeed) {
+        this.model = model;
+        this.price = price;
+        this.maxSpeed = maxSpeed;
+    }
+
+
+    @Override
+    public int compareTo(Car other) {
+        // return this.price - other.price; - Asc order of price
+        // Desc order of price -
+        // return other.price - this.price;
+        // comparison on the basis of Model
+        // return this.model.compareTo(other.model);
+        return other.model.compareTo(this.model);
+    }
+
+    public String toString() {
+        return this.model + "\t" + this.price + "\t" + this.maxSpeed;
+    }
+}
